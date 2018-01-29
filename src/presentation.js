e84b01be52e12a6d168e87888aa9d584805e93a5
Bernardo Pacheco <berna.ipach@gmail.com>
2018-01-28 23:59:46 -0500

    Working on first slide

--- src/presentation.js
+++ src/presentation.js
@@ -2,7 +2,9 @@
 import React from "react"
 
 // Import Spectacle Core tags
-import { BlockQuote, Cite, Deck, Heading, ListItem, List, Quote, Slide, Text } from "spectacle"
+import { Deck, Heading, Image, Slide, Text } from "spectacle"
+
+import preloader from "spectacle/lib/utils/preloader"
 
 // Import theme
 import createTheme from "spectacle/lib/themes/default"
@@ -10,12 +12,18 @@ import createTheme from "spectacle/lib/themes/default"
 // Require CSS
 require("normalize.css")
 
+const images = {
+    dpzLogo: require("./images/dpz-logo.png"),
+}
+
+preloader(images)
+
 const theme = createTheme(
     {
-        primary: "white",
-        secondary: "#1F2022",
-        tertiary: "#03A9FC",
-        quartenary: "#CECECE",
+        primary: "#0078ae",
+        secondary: "#fff",
+        tertiary: "#e31837",
+        quaternary: "#333",
     },
     {
         primary: "Montserrat",
@@ -27,53 +35,34 @@ export default class Presentation extends React.Component {
     render() {
         return (
             <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
-                <Slide transition={["zoom"]} bgColor="primary">
-                    <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-                        Spectacle Boilerplate
+                <Slide transition={["zoom"]} bgColor="secondary">
+                    <Heading size={3} lineHeight={1} textColor="primary">
+                        ASTronomically Improving Your Developer Experience
                     </Heading>
-                    <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-                        open the presentation/index.js file to get started
+                    <Heading size={5} fit lineHeight={1} textColor="tertiary">
+                        (A JS Abstract Syntax Trees Primer)
+                    </Heading>
+                    <Text
+                        margin="100px 0 0"
+                        size={6}
+                        textAlign="right"
+                        textColor="quaternary"
+                        textFont="secondary"
+                    >
+                        Bernardo Pacheco
                     </Text>
-                </Slide>
-                <Slide transition={["fade"]} bgColor="tertiary">
-                    <Heading size={6} textColor="primary" caps>
-                        Typography
-                    </Heading>
-                    <Heading size={1} textColor="secondary">
-                        Heading 1
-                    </Heading>
-                    <Heading size={2} textColor="secondary">
-                        Heading 2
-                    </Heading>
-                    <Heading size={3} textColor="secondary">
-                        Heading 3
-                    </Heading>
-                    <Heading size={4} textColor="secondary">
-                        Heading 4
-                    </Heading>
-                    <Heading size={5} textColor="secondary">
-                        Heading 5
-                    </Heading>
-                    <Text size={6} textColor="secondary">
-                        Standard text
+                    <Text size={6} textAlign="right" textColor="quaternary" textFont="secondary">
+                        <Image
+                            src={images.dpzLogo.replace("/", "")}
+                            height="40px"
+                            margin="0"
+                            display="inline"
+                        />{" "}
+                        Front-End Developer
                     </Text>
-                </Slide>
-                <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-                    <Heading size={6} textColor="secondary" caps>
-                        Standard List
-                    </Heading>
-                    <List>
-                        <ListItem>Item 1</ListItem>
-                        <ListItem>Item 2</ListItem>
-                        <ListItem>Item 3</ListItem>
-                        <ListItem>Item 4</ListItem>
-                    </List>
-                </Slide>
-                <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-                    <BlockQuote>
-                        <Quote>Example Quote</Quote>
-                        <Cite>Author</Cite>
-                    </BlockQuote>
+                    <Text size={6} textAlign="right" textColor="quaternary" textFont="secondary">
+                        @bernardop
+                    </Text>
                 </Slide>
             </Deck>
         )
